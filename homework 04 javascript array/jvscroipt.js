6fdd28dee20d44aa7dc83b80419215678cdd10f7
nevenkaCuleva <77502052+nevenkaCuleva@users.noreply.github.com>
2021-01-21 14:02:52 +0100

    Add files via upload

---
+++ homework 04 javascript array/jvscroipt.js
@@ -0,0 +1,28 @@
+// HOMEWROK #1
+
+
+let stringArray= ["John", "sleeping", "happy", "exploring", "curious", "Mary"];
+
+
+
+function tellStory(name, mood, activity){
+    console.log(`This is ${name}. ${name} is a nice person. Today they are ${mood}. They are ${activity} all day. The end.`)
+}
+
+tellStory((stringArray[0]), (stringArray[2]), (stringArray[1]));
+tellStory((stringArray[5]), (stringArray[4]), (stringArray[3]))
+
+
+// HOMEWORK #2
+
+
+function calculateSum(num1, num2, num3, num4, num5){
+    console.log(`The sum of your numbers is ${((num1) + (num2) + (num3) + (num4) + (num5))}`);
+}
+
+calculateSum(5, 2, 3, 10, 1);
+
+
+
+
+
